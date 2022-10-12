8ef59080466263ec3e9dcf7b184865f80718369e
EDUARDO NIENKOTTER MARCELINO <eduardo_n_marcelino@estudante.sesisenai.org.br>
2022-10-11 22:12:40 -0300

    Foi adicionado comentários para as linhas de código

--- pokemon.js
+++ pokemon.js
@@ -1,14 +1,19 @@
+//Criação de vetor e subvetores para cada região Pokémon
 let regiao = [[], [], [], [], [], [], []]
 
+//Estrutura de Repetição para perguntar o Pokémon encontrado
 for (let poke = 0; poke < 7; poke++) {
   while (true) {
     let pokemon = prompt(`Digite o pokemon encontrado de geração ${poke + 1}.
-     Digite 'next' para pular para a próxima Geração.`)
+     Digite 'next' para ir até a próxima Geração.`)
 
+//Condiconal para seguir para a próxima geração
     if (pokemon.toUpperCase() === `NEXT`) {
       break
     } else regiao[poke].push(pokemon)
   }
+
+  //Switch criado para definir o nome de cada região baseada em seu índice no vetor
   var geracao
   switch (poke) {
     case 0: geracao = "Kanto"; break
@@ -21,13 +26,15 @@ for (let poke = 0; poke < 7; poke++) {
   }
   console.log(`Os Pokemon de ${geracao} são:`)
 
+//Estrutura de repetição que irá mostrar todos os Pokémon encontrados em cada geração
   for (let numPoke = 0; regiao[poke][numPoke] !== undefined; numPoke++)
     console.log(`${regiao[poke][numPoke]}`)
 }
+//Descontração final com uma condicional para saber o seu Pokémon favorito
 let fav = prompt("Qual seu Pokemon favorito?")
 
-if (fav !== "Kyogre"){
+if (fav !== "Kyogre") {
   console.log(`Seu Pokémon favorito é ${fav}? Péssima escolha...`)
-}else {
+} else {
   console.log("Você é uma pessoa de cultura escolhendo o Kyogre!")
 }
