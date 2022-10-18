c42b8167d0cdc498e691c790a8a910579617dd03
Eduardo Nienkotter Marcelino <eduardo_n_marcelino@estudante.sesisenai.org.br>
2022-10-17 21:17:53 -0300

    Trabalho concluido com sucesso

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,17 @@
 let frm = document.querySelector("form")
-let respReg = document.querySelector("#regiao")
-let respPoke = document.querySelector("#poke")
-let respFav = document.querySelector("#fav")
+let saidaKanto = document.getElementById("regiaoKanto")
+let saidaPokeKanto = document.getElementById("pokeKanto")
+let saidaJohto = document.getElementById("regiaoJohto")
+let saidaPokeJohto = document.getElementById("pokeJohto")
+let saidaHoenn = document.getElementById("regiaoHoenn")
+let saidaPokeHoenn = document.getElementById("pokeHoenn")
+let saidaSinoh = document.getElementById("regiaoSinoh")
+let saidaPokeSinoh = document.getElementById("pokeSinoh")
+let saidaUnova = document.getElementById("regiaoUnova")
+let saidaPokeUnova = document.getElementById("pokeUnova")
+let saidaKalos = document.getElementById("regiaoKalos")
+let saidaPokeKalos = document.getElementById("pokeKalos")
+let saidaFav = document.querySelector("#fav")
 
 frm.addEventListener("click", (e) => {
   //previne o envio do Formulario
@@ -11,42 +21,60 @@ frm.addEventListener("click", (e) => {
   let geracao = [[], [], [], [], [], []]
 
   //Laço de repetição que pergunta os nomes dos Pokémon encontrados:
-  for (let numPoke = 0; numPoke < 6; numPoke++) {
+  for (let numRegiao = 0; numRegiao < 6; numRegiao++) {
     while (true) {
-      let pokemon = prompt(`Digite o Pokémon encontrado na geração ${numPoke + 1}.
-     Digite 'next' para ir até a próxima geração.`)
+      let pokemon = prompt(`Digite o Pokémon encontrado na geração ${numRegiao + 1}.`)
 
       //Condicional para seguir para a próxima geração ou colocar no subvetor o nome do Pokémon:
       if (pokemon.toUpperCase() === `NEXT`) {
         break
-      } else geracao[numPoke].push(pokemon)
+      } else geracao[numRegiao].push(pokemon)
     }
-
-    //Condicional que define o nome de cada região baseada em seu Indice Vetorial:
-    let regiao
-    switch (numPoke) {
-      case 0: regiao = "Kanto"; break
-      case 1: regiao = "Johto"; break
-      case 2: regiao = "Hoenn"; break
-      case 3: regiao = "Sinoh"; break
-      case 4: regiao = "Unova"; break
-      case 5: regiao = "Kalos"; break
-    }
-
-    respReg.innerHTML = `Os Pokémon encontrados em ${regiao} são:` + "<br>"
-
-    //Laço de repetição para mostrar todos os Pokémon encontrados
-    for (let poke = 0; geracao[numPoke][poke] !== undefined; poke++){
-        respPoke.innerHTML = `${geracao[numPoke][poke]}` + "<br>"
-      }
-    
   }
+
+  //Impressão dos pokemon da região de Kanto
+  saidaKanto.innerHTML = `Os Pokémon encontrados em Kanto são:` + "<br>"
+  //Laço de repetição para mostrar todos os Pokémon encontrados
+  for (const pokes of geracao[0]) {
+    saidaPokeKanto.innerText += `${pokes}, `
+  }
+  //Impressão dos pokemon da região de Johto
+  saidaJohto.innerHTML = `Os Pokémon encontrados em Johto são:` + "<br>"
+  //Laço de repetição para mostrar todos os Pokémon encontrados
+  for (const pokes of geracao[1]) {
+    saidaPokeJohto.innerText += `${pokes}, `
+  }
+  //Impressão dos pokemon da região de Hoenn
+  saidaHoenn.innerHTML = `Os Pokémon encontrados em Hoenn são:` + "<br>"
+  //Laço de repetição para mostrar todos os Pokémon encontrados
+  for (const pokes of geracao[2]) {
+    saidaPokeHoenn.innerText += `${pokes}, `
+  }
+  //Impressão dos pokemon da região de Sinoh
+  saidaSinoh.innerHTML = `Os Pokémon encontrados em Sinoh são:` + "<br>"
+  //Laço de repetição para mostrar todos os Pokémon encontrados
+  for (const pokes of geracao[3]) {
+    saidaPokeSinoh.innerText += `${pokes}, `
+  }
+  //Impressão dos pokemon da região de Unova
+  saidaUnova.innerHTML = `Os Pokémon encontrados em Unova são:` + "<br>"
+  //Laço de repetição para mostrar todos os Pokémon encontrados
+  for (const pokes of geracao[4]) {
+    saidaPokeUnova.innerText += `${pokes}, `
+  }
+  //Impressão dos pokemon da região de Kalos
+  saidaKalos.innerHTML = `Os Pokémon encontrados em Kalos são:` + "<br>"
+  //Laço de repetição para mostrar todos os Pokémon encontrados
+  for (const pokes of geracao[5]) {
+    saidaPokeKalos.innerText += `${pokes}, `
+  }
+
   //Descontração final com uma condicional para saber o seu Pokémon Lendário favorito
   let fav = prompt("Qual seu Pokemon Lendário favorito?")
 
   if (fav.toUpperCase() !== "KYOGRE") {
-    respFav.innerHTML = `Seu Lendário favorito é ${fav}? Péssima escolha...`
+    saidaFav.innerHTML = `Seu Lendário favorito é ${fav}? Péssima escolha...`
   } else {
-    respFav.innerHTML = `Você é uma pessoa de cultura escolhendo o Kyogre!`
+    saidaFav.innerHTML = `Você é uma pessoa de cultura escolhendo o Kyogre!`
   }
 })
