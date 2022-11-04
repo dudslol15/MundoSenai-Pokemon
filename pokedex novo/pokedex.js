c6acaef5209e809de0b451f0e0c8ed83a29ac30d
GABRIEL NASCIMENTO PINTO <gabriel_n_pinto@estudante.sesisenai.org.br>
2022-11-03 22:17:21 -0300

    Começo da atualização final

---
+++ pokedex novo/pokedex.js
@@ -0,0 +1,14 @@
+let pokeball = new Audio ("../sound/Pokeball_SoundMP3_320K.mp3")
+let pokemons = document.querySelector("#pokemons")
+let snolax = document.querySelector("#Snolax")
+let mew = document.querySelector("#mew")
+let teste = document.getElementById("teste")
+
+
+
+//teste.addEventListener("click", tocar)
+function tocar() {
+    console.log("foi")
+    pokeball.play();
+
+}
