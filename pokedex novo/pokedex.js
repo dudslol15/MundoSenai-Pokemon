33db6c7b0b0e7df1842efdbe1f447ddc823f1e19
NIKOLAS RODRIGUES DA CRUZ <nikolas_cruz@estudante.sesisenai.org.br>
2022-11-07 22:17:06 -0300

    final up

---
+++ pokedex novo/pokedex.js
@@ -0,0 +1,13 @@
+let pokeball = new Audio("../sound/pokeball.mp3")
+let pokemons = document.querySelector("#pokemons")
+let snolax = document.querySelector("#Snolax")
+let mew = document.querySelector("#Mew")
+let teste = document.getElementById("teste")
+
+//teste.addEventListener("click", tocar)
+function tocar() {
+    console.log("foi")
+    pokeball.play();
+
+
+}
